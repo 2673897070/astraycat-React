7387f72e20d4735bb35e0bc5f535a828974ea7f4
2673897070 <2673897070@qq.com>
2020-01-02 14:26:01 +0800

    gengai

---
+++ src/html/RoQi/HomePage/FindMusic/LeaderBoard/xiangqing/xiangqing.js
@@ -0,0 +1,77 @@
+import React from 'react'
+import { Card } from 'antd';
+import { Button, Radio, Icon, notification } from 'antd';
+import Xiangqingcss from './xiangqing.css';
+import Featuredless from './featured.less';
+import { Link } from 'react-router-dom';
+
+const { Meta } = Card;
+var Xiangqingcss = require('./xiangqing.css')
+export default class Product extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newsItem: [
+                {
+                    formName: "",
+                    username: "",
+                    musicName: "",
+                    singer: "",
+                    album: "",
+                    allTime: "",
+                    collection: ""
+
+                }
+            ]
+        }
+    }
+
+    update(){
+        var data = {
+            "fromId": 1,
+            "userId": 1,
+        }
+        //fetch
+        fetch("/musicForm/musicInfo", {
+            method: "post",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(data)
+        }).then(response => response.json())
+            .then(result => {
+                console.log("0");
+                console.log("0");
+                console.log(result);
+                console.log(result);
+                this.setState({ newsItem: result });
+            })
+    }
+
+    render() {
+        // console.log(this.props.product);
+        const { newsItem } = this.state;
+        return (
+
+            <div>
+                {
+                    newsItem.map((Item, index) => (
+                        <div>
+                            <Card className={Xiangqingcss.Cd}
+                                cover={<img src={"this.props.product1"} className={Xiangqingcss.Card} />}
+                            ></Card>
+                            <span className={Xiangqingcss.anniu}>
+                                <Button type="primary" shape="round" icon="step-forward" size="large">播放</Button>
+                            </span>
+                            <span className={Xiangqingcss.anniu}>
+                                <Button type="primary" shape="round" icon="plus-circle" size="large">收藏</Button>
+                            </span>
+                            <span className={Xiangqingcss.anniu}>
+                                <Button type="primary" shape="round" icon="vertical-align-bottom" size="large">下载</Button>
+                            </span></div>
+                    ))
+                }
+            </div>
+        )
+    }
+}
